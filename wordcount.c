14f5f9c175f5ef07d9a711ff752c5a74ecf3257b
Simon Sedlacek <saimon.sedlacek@gmail.com>
2019-03-29 14:09:17 +0100

    adding files

---
+++ wordcount.c
@@ -0,0 +1,15 @@
+/**
+ *	@file	wordcount.c
+ *	@author	Simon Sedlacek, FIT
+ *	@date	29.3.2019
+ *	@note	Reseni: IJC-DU2, priklad 2)
+ *	Prelozeno: gcc 8.3.1 - Fedora release 29 (Twenty Nine) x86_64 
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+
+    return 0;
+}

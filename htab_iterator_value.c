24c5695727e8077808985eaa2c2f570dbb44f492
Simon Sedlacek <saimon.sedlacek@gmail.com>
2019-04-25 23:21:49 +0200

    added assertions to get_value and set_value

--- htab_iterator_value.c
+++ htab_iterator_value.c
@@ -7,18 +7,16 @@
  */
 
 #include <stdlib.h>
+#include <assert.h>
 #include "htab.h"
 #include "htab_private.h"
 
 int htab_iterator_get_value(htab_iterator_t it) {
-    if (it.ptr == NULL)
-        return 0;
+    assert(it.ptr != NULL);
     return it.ptr->data;
 }
 
 int htab_iterator_set_value(htab_iterator_t it, int val) {
-    if (it.ptr == NULL)
-        return -1;
-    it.ptr->data = val;
-    return 0;
+    assert(it.ptr != NULL);
+    return it.ptr->data = val;
 }
